8b61b575f02c226cb6d87b452566d8e651b75d2d
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2021-09-28 11:30:39 +0200

    config(cmake): added new files

--- src/client.cc
+++ src/client.cc
@@ -6,24 +6,8 @@ int
 main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv);
 
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  char port[MPI_MAX_PORT_NAME];
-
-  MPI_Lookup_name("server", MPI_INFO_NULL, &port[0]);
-
-  std::cout << "client " << rank << " found port: " << port << std::endl; 
-
-  MPI_Comm interComm;
-  MPI_Comm_connect(port, MPI_INFO_NULL, rank, MPI_COMM_SELF, &interComm);
-
-  std::cout << "client " << rank << " connected." << std::endl; 
-  std::cout << "client " << rank << " shutting down." << std::endl; 
-
-  MPI_Comm_disconnect(&interComm);
-
   MPI_Finalize();
+
   return 0;
 }
 
--- src/server.cc
+++ src/server.cc
@@ -1,9 +1,6 @@
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <cstdlib>
-#include <unistd.h>
-#include "mpi.h"
+#include <mpi.h>
+
+#include "node.hh"
 
 int
 main(int argc, char* argv[]) {
@@ -11,35 +8,25 @@ main(int argc, char* argv[]) {
 
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  char port[MPI_MAX_PORT_NAME];
-  MPI_Open_port(MPI_INFO_NULL, &port[0]);
-  
-  MPI_Info publishInfo;
-  MPI_Info_create(&publishInfo);
-  MPI_Info_set(publishInfo, "ompi_global_scope", "true");
-  MPI_Info_set(publishInfo, "ompi_unique", "true");
+  Node node(rank, size);
 
-  std::cout << "server " << rank << " openend port: " << port << std::endl; 
+  if (node.getNodeId() == 4) {
+    node.startElection();
+  } else {
+    int srcNodeId;
+    Message message;
+    node.receiveMessage(srcNodeId, message);
 
-  std::cout << "server " << rank << " publishing port: " << port << std::endl; 
-  MPI_Publish_name("server", publishInfo, port);
-
-  std::cout << "server " << rank << " waiting for connection." << std::endl; 
-  MPI_Comm interComm;
-  MPI_Comm_accept(port, MPI_INFO_NULL, 0, MPI_COMM_SELF, &interComm);
-
-  std::cout << "server " << rank << " connected." << std::endl; 
-
-  MPI_Info unpublishInfo;
-  MPI_Info_create(&unpublishInfo);
-  MPI_Info_set(unpublishInfo, "ompi_global_scope", "true");
-
-  std::cout << "server " << rank << " shutting down." << std::endl; 
-  MPI_Unpublish_name("server", unpublishInfo, port);
-
-  MPI_Close_port(port);
+    std::cout << "node: " << node.getNodeId()
+              << " received message with tag: " << message.tag
+              << " and code: " << message.code << " from node: " << srcNodeId
+              << std::endl;
+  }
 
   MPI_Finalize();
+
   return 0;
 }
